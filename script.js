232550f43140bc6778247fda3242871775d1a524
alina vergunova <alinadeveloper84@gmail.com>
2023-10-22 17:18:36 +0300

    adaptive 1440-

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
   "use strict" ;
 
-  console.log("load script");
-function priceDropDown() {
-    console.log('Basics click');
+  const buttonAcc = document.getElementsByClassName("accordion");
+  for (let i = 0; i < buttonAcc.length; i++) {
+    buttonAcc[i].addEventListener("click", function() {
+      buttonAcc[i].classList.toggle("active-accordion");
+
+      const wrapper = buttonAcc[i].nextElementSibling;
+      wrapper.classList.toggle("active-wrapper");
+    })
   }
+
 });
