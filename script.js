ca1e378bf86c95dc3ffdd99d7635902ef807d7f8
alina vergunova <alinadeveloper84@gmail.com>
2023-10-24 20:44:18 +0300

    accordion-wrapper price

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   "use strict" ;
-
+  // City accordion
   const buttonAcc = document.getElementsByClassName("accordion");
   for (let i = 0; i < buttonAcc.length; i++) {
     buttonAcc[i].addEventListener("click", function() {
@@ -11,4 +11,24 @@ $(document).ready(function() {
     })
   }
 
+  // Price accordion-wrapper
+  const wrappers = document.getElementsByClassName('accordion-wrapper-btn');
+  function removeAllActive() {
+    for (let i = 0; i < wrappers.length; i++) {
+        wrappers[i].classList.remove('accordion-wrapper-btn-active');
+        wrappers[i].firstElementChild.nextElementSibling.classList.remove('accordion-wrapper-line-active');
+      };
+  }
+  for (let i = 0; i < wrappers.length; i++) {
+    wrappers[i].addEventListener('click', function() {
+      if (wrappers[i].classList.contains('accordion-wrapper-btn-active')) {
+        wrappers[i].classList.remove('accordion-wrapper-btn-active');
+        wrappers[i].firstElementChild.nextElementSibling.classList.remove('accordion-wrapper-line-active');
+      } else {
+        removeAllActive();
+        wrappers[i].classList.add('accordion-wrapper-btn-active');
+        wrappers[i].firstElementChild.nextElementSibling.classList.add('accordion-wrapper-line-active');
+      }
+    });
+    };
 });
